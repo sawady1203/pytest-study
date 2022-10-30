463efcbe16e08c20e87d55535f02d02ff4af98fb
sawady1203 <sawady.dev@gmail.com>
2022-10-30 21:53:11 +0900

    2022/10/30 2.3.1

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -5,12 +5,11 @@ def test_sample():
     assert app.sample.countdown(2) == 1
     assert app.sample.countdown(0) == None
 
-def test_countdown_file():
-    with tempfile.TemporaryDirectory() as tmpdir:
-        filename = f"{tmpdir}/contdown.txt"
-        with  open(filename, "w") as fp:
-            fp.write("4")
-        assert app.sample.countdown_file(filename) == 3
-        with open(filename, "r") as fp:
-            body = fp.read()
-        assert body  == "4\n3"
+def test_countdown_file(tmp_path):
+    filename = f"{tmp_path}/contdown.txt"
+    with  open(filename, "w") as fp:
+        fp.write("4")
+    assert app.sample.countdown_file(filename) == 3
+    with open(filename, "r") as fp:
+        body = fp.read()
+    assert body  == "4\n3"
